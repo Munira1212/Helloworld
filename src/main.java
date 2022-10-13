2040e54e4c8fabe45883c35b33c6f3b82c5b8d2d
Munira1212 <muni0301@stud.kea.dk>
2022-10-13 11:32:37 +0200

    mm

---
+++ src/main.java
@@ -0,0 +1,9 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+public class main {
+    public static void main(String[] args) {
+     System.out.println("Hello world");
+        System.out.println("");
+    }
+    }
+
